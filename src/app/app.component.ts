c6ba1df22deba0f70b55ab66dd3a756a610b91b7
Gaurav Khatwani <gkhatwani@microsoft.com>
2024-03-26 23:37:29 +0530

    changed project name

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import { Title, Meta } from '@angular/platform-browser';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit{
-  title = 'andresjosehr-portfolio';
+  title = 'gauravkhatwani-portfolio';
   
   constructor(
     private titleService: Title,

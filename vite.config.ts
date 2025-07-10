e8e03f7e3f856fead41334bbf412783693c49693
sakshat-patil <sakshatpatil25@gmai.com>
2025-07-10 14:06:41 -0700

    icon added

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/sakshat-patil.github.io/', // Set to your repo name
+  // base: '/sakshat-patil.github.io/', // Comment out for root deployment
   plugins: [react()],
 })
